323865df4384499f08855918c414d4ffd943e75d
KiranRevanna1 <kiranrevanna1@gmail.com>
2025-09-23 06:38:35 +0530

    fix

--- frontend/src/services/socket.ts
+++ frontend/src/services/socket.ts
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const SERVER_URL = process.env.VITE_SERVER_URL || "http://localhost:4000";
+const SERVER_URL = process.env.REACT_APP_SERVER_URL || "http://localhost:4000";
 
 export const socket = io(SERVER_URL, {
   transports: ['websocket'],
